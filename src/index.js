8502097786313f1a938649b502dfb7d26ca5ac30
kpbongat <kpbongat.dev@gmail.com>
2024-12-08 18:31:37 +0800

    Create server to serve basic pages

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-import "./style.css";
+const http = require("http");
+const fs = require("fs");
+
+http
+  .createServer((req, res) => {
+    const reqUrl = new URL(
+      `http://${req.headers.host ?? "localhost"}${req.url}`
+    );
+    const path = reqUrl.pathname;
+    const fileName = path === "/" ? "./index.html" : `.${path}.html`;
+    fs.readFile(fileName, (err, data) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("404");
+        return;
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  })
+  .listen(8080);
